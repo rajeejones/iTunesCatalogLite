c9b3c67cb2ce2dcf680db74b8787ed329280d72d
Rajee Jones <rajee.jones@aa.com>
2020-04-10 11:16:22 -0500

    updated package to support v3.0.0 with tvOS target

---
+++ iTunesCatalogLite/iTunesCatalogTV/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  iTunesCatalogTV
+//
+//  Created by Rajee Jones on 4/10/20.
+//  Copyright © 2020 OldMoonStudios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    HStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    HStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
